a476c282f1599012b14ed927ded1fb8820fc6ff5
pedro-c-aquino <pedro.aquino@foursys.com.br>
2022-09-27 10:51:55 -0700

    Created network manager get movie method

--- DesafioMobile2019Teste14/Network/NetworkManeger.swift
+++ DesafioMobile2019Teste14/Network/NetworkManeger.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+class NetworkManager {
+    
+    private let apiKey = "9a8eb9c907c28bf818a89a58604c8b9a"
+    
+    func getMovies<T: Codable> (movieType: String, completionHandler: @escaping (T) -> Void) {
+        
+        guard let url = URL(string: "https://api.themoviedb.org/3/movie/\(movieType)?api_key=\(apiKey)") else { return }
+        
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data, error == nil else {
+                print(error?.localizedDescription)
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(T.self, from: data)
+                  completionHandler(result)
+            } catch {
+                print(error.localizedDescription)
+
+            }
+        }
+        task.resume()
+    }
+}
--- DesafioMobile2019Teste14/Upcoming/UpcomingView.swift
+++ DesafioMobile2019Teste14/Upcoming/UpcomingView.swift
@@ -8,11 +8,20 @@
 import UIKit
 
 class UpcomingView: UIViewController {
+    
+    private var resultArray: [Result] = []
+    
+    private let networkManager = NetworkManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .red
+        
+        networkManager.getMovies(movieType: "popular") { (popularMovies: PopularModel) in
+            print(popularMovies)
+        }
+        
         // Do any additional setup after loading the view.
     }
 
