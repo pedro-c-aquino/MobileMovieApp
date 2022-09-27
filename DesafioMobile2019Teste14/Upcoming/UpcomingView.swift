ad121ac780bd42c936245410c3dc997a66680834
Vinicius Angelo <vinicius.angelo@foursys.com.br>
2022-09-27 19:25:32 -0300

    create collectionView

--- DesafioMobile2019Teste14/Upcoming/UpcomingView.swift
+++ DesafioMobile2019Teste14/Upcoming/UpcomingView.swift
@@ -27,10 +27,21 @@ class UpcomingView: UIViewController {
         return label
     }()
     
+    private lazy var collectionView: UICollectionView = {
+       let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.backgroundColor = .green
+        collection.delegate = self
+        collection.dataSource = self
+        collection.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        return collection
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //navigationController?.navigationBar.backgroundColor = .red
         view.backgroundColor = #colorLiteral(red: 0.1215686426, green: 0.1215686426, blue: 0.1215686426, alpha: 1)
         
         setupViews()
@@ -41,6 +52,7 @@ class UpcomingView: UIViewController {
         
         view.addSubview(segmentedControl)
         view.addSubview(titleLabel)
+        view.addSubview(collectionView)
         
     }
     
@@ -56,6 +68,11 @@ class UpcomingView: UIViewController {
             titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
             titleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
         
+            collectionView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 5),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.heightAnchor.constraint(equalTo: collectionView.widthAnchor, multiplier: 0.5)
+
         ])
     }
     
@@ -67,5 +84,23 @@ class UpcomingView: UIViewController {
             titleLabel.text = "Popular Movies"
         }
     }
+}
+
+extension UpcomingView: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width/2.5, height: collectionView.frame.width/2)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
 }
 
