11016558148303ee598b7c83ccb59eba3ceef348
Thiago Fernandes <thiago.fernandes@foursys.com.br>
2022-09-28 06:27:53 -0700

    SceneDelegate Details to UpcomingView

--- DesafioMobile2019Teste14/AppDelegate/SceneDelegate.swift
+++ DesafioMobile2019Teste14/AppDelegate/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
             guard let windowScene = (scene as? UIWindowScene) else { return }
             let window = UIWindow(windowScene: windowScene)
-            let rootViewController = DetailsView()
+            let rootViewController = UpcomingView()
             let navigationController = UINavigationController()
         
             navigationController.navigationBar.prefersLargeTitles = true
